48d415525b2333eddb935469a422f649351d5550
BenoitGaudieri <benalek@gmail.com>
2020-03-30 14:01:52 +0200

    feat. dice roll completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+// var title = document.getElementById("title");
+// var titletext = title.innerHTML;
+// var modTitleText = "";
+
+// for (var i = 0; i < titletext.length; i++) {
+//     modTitleText +=
+//         '<span class="char-' + (i + 1) + '">' + titletext[i] + "</span>";
+// }
+
+// title.innerHTML = modTitleText;
+
 // Declaration
 var submit = document.getElementById("submit");
 var arr = [
@@ -29,3 +40,23 @@ submit.addEventListener("click", function() {
 });
 
 // Random
+var rollDice = document.getElementById("rollDice");
+
+rollDice.addEventListener("click", function() {
+    var rollResult = document.getElementById("rollResult");
+    var playerRoll = Math.floor(Math.random() * 6) + 1;
+    var pcRoll = Math.floor(Math.random() * 6) + 1;
+    console.log(playerRoll);
+    console.log(pcRoll);
+
+    if (playerRoll > pcRoll) {
+        rollResult.className = "show";
+        rollResult.innerHTML = `The player rolled ${playerRoll} while the pc rolled ${pcRoll}<p>Player wins!</p>`;
+    } else if (playerRoll < pcRoll) {
+        rollResult.className = "show";
+        rollResult.innerHTML = `The player rolled ${playerRoll} while the pc rolled ${pcRoll}<p>PC wins!</p>`;
+    } else {
+        rollResult.className = "show";
+        rollResult.innerHTML = `The player rolled ${playerRoll} while the pc rolled ${pcRoll}<p>It's a draw!</p>`;
+    }
+});
